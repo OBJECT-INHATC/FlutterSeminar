78500e63c534f3550e767fa926093bec80d7e325
Pinkippo <hseungwan8@gmail.com>
2023-08-07 14:06:42 +0900

    feat(firebase): 파이어베이스 연결

--- providerpattern/lib/main.dart
+++ providerpattern/lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:providerpattern/firebase_options.dart';
 
 import 'models/auth_model.dart';
 import 'services/auth_service.dart';
 import '/screens/login_screen.dart';
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MaterialApp(
       title: 'Provider Example',
